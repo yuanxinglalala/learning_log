e748015762401794fdb2b514a82e728f88f34cf7
yuanxing <1908548765@qq.com>
2020-03-20 19:47:11 +0800

    functional test now checks ,we can input a To-DO item

--- functiontest.py
+++ functiontest.py
@@ -1,13 +1,31 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest  #导入单元测试模块
 class NewVisitorTest(unittest.TestCase): 
 	def setUp(self):           #在测试方法之前运行，打开浏览器
 		self.browser = webdriver.Firefox()
 	def tearDown(self): 	   #在测试方法之后运行，关闭浏览器
 		self.browser.quit()
-	def test_to_do(self): 
+	def test_can_start_a_list_and_retrieve_it_later(self): 
 		self.browser.get('http://localhost:8000')
-		self.assertIn('To-Do', self.browser.title) 
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text 
+		self.assertIn('To-Do', header_text)
+		
+		inputbox = self.browser.find_element_by_id('id_new_item') 
+		self.assertEqual(inputbox.get_attribute('placeholder'),
+				'Enter a to-do item')
+		inputbox.send_keys('Buy peacock feathers') 
+		
+		inputbox.send_keys(Keys.ENTER) 
+		time.sleep(1) 
+		
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr') 
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
 		self.fail('Finish the test!')    #失败后返回指定的错误信息  
 
 #使用这个语句检查自己是否在命令行中运行，而不是在其他脚本中导
