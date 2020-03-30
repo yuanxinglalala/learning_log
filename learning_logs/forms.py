740fe79ef46ddbbd752055dbd9a3f7c3348a5aca
yuanxing <1908548765@qq.com>
2020-03-30 23:09:44 +0800

    send post request to add new topic
    -发送post请求来添加新主题

---
+++ learning_logs/forms.py
@@ -0,0 +1,10 @@
+# forms.py
+from django import forms
+
+from .models import Topic
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = ['text']
+        labels = {'text': ''}
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
 from .models import Topic
-
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 # Create your views here.
 
 def index(request):
@@ -19,3 +20,19 @@ def topic(request, topic_id):
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
+	
+def new_topic(request):
+	"""添加新主题"""
+	if request.method != 'POST':
+		# 未提交数据：创建一个新表单
+		form = TopicForm()
+	else:
+		# POST提交的数据,对数据进行处理
+		form = TopicForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('learning_logs:topics'))
+	context = {'form': form}
+	return render(request, 'learning_logs/new_topic.html', context)
+
+
