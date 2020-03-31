eff89b4a982c15440cb8eb6b492eae09c432cd39
yuanxing <1908548765@qq.com>
2020-03-31 21:11:20 +0800

    add new entry and edit entries
    -创建添加新条目和编辑条目的页面

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,10 +1,16 @@
 # forms.py
 from django import forms
-
-from .models import Topic
+from .models import Topic, Entry
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
         labels = {'text': ''}
+
+class EntryForm(forms.ModelForm):
+	class Meta:
+		model = Entry
+		fields = ['text']
+		labels = {'text': ''}
+		widgets = {'text': forms.Textarea(attrs={'cols': 80})}  #添加行输入控件
--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,6 +14,10 @@ urlpatterns = [
 	url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 	# 用于添加新主题的网页
 	url(r'^new_topic/$', views.new_topic, name='new_topic'),
-	 # add new topic page
+	 # 用于添加新主题
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    # 用于添加新条目的页面
+	url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+	#用于编辑条目
+	url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry,name='edit_entry'),
 ]
